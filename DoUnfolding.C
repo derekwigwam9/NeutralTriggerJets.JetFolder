90124be8b3ddfff8cdc5231d3e8895c29840e84e
Derek Anderson <dmawxc@pdsf8.nersc.gov>
2018-07-02 13:19:43 -0700

    fixed the bounds on the chi2 = 1 line and the SVD performance plots

--- DoUnfolding.C
+++ DoUnfolding.C
@@ -34,8 +34,11 @@
 #include <TSystem>
 #include <iostream>
 #include "TMath.h"
+#include "TLine.h"
 #include "TString.h"
 #include "TDatime.h"
+#include "TCanvas.h"
+#include "TLegend.h"
 #include "TPaveText.h"
 
 using namespace std;
@@ -45,50 +48,50 @@ class StJetFolder;
 
 
 // input and output files
-const TString  pFile("input/pp200r9embed.unfoldingPtBin1.et9vz55.r03a02rm1chrg.dr03q15.root");
-const TString  sFile("input/pp200r9embed.unfoldingPtBin1.et9vz55.r03a02rm1chrg.dr03q15.root");
-const TString  mFile("input/pp200r9.forUnfoldingPtBin1.et920vz55.r03a02rm1chrg.root");
-const TString  eFile("input/pp200r9embed.unfoldingPtBin1.et9vz55.r03a02rm1chrg.dr03q15.root");
-const TString  rFile("input/pp200r9embed.unfoldingPtBin1.et9vz55.r03a02rm1chrg.dr03q15.root");
-const TString  oFile("pp200r9.unfolding.et9vz55.r03a02rm1chrg.dr03q15");
+static const TString  pFile("input/pp200r9embed.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.et9vz55.r03a02rm1chrg.dr03q15.root");
+static const TString  sFile("input/pp200r9embed.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.et9vz55.r03a02rm1chrg.dr03q15.root");
+static const TString  mFile("input/pp200r9embed.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.et9vz55.r03a02rm1chrg.dr03q15.root");
+static const TString  eFile("input/pp200r9embed.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.et9vz55.r03a02rm1chrg.dr03q15.root");
+static const TString  rFile("input/pp200r9embed.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.et9vz55.r03a02rm1chrg.dr03q15.root");
+static const TString  oFile("pp200r9.withCorrectDfAndTrgCutsAndCorrectDetectorSpectrum.pTbinVar.ClosureALLxALL.et9vz55.r03a02rm1chrg.dr03q15");
 // input namecycles
-const TString  pName("hSumParAll");
-const TString  sName("hSumDetAll");
-const TString  mName("Pi0/hJetPtCorrP");
-const TString  eName("hEfficiencyAll");
-const TString  rName("hResponseAll");
+static const TString  pName("hSumParAll");
+static const TString  sName("hSumDetAll");
+static const TString  mName("hSumDetAll");
+static const TString  eName("hEfficiencyAll");
+static const TString  rName("hResponseAll");
 
 // unfolding parameters (to loop over)
-const Int_t nM  = 2;
-const Int_t M[] = {1, 2};
-const Int_t nK  = 30;
-const Int_t K[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+static const Int_t nM  = 2;
+static const Int_t M[] = {1, 2};
+static const Int_t nK  = 15;
+static const Int_t K[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 // prior parameters (to loop over)
-const Int_t    P   = 0;
-const Int_t    nN  = 1;
-const Double_t N[] = {5.8};
-const Double_t nT  = 1;
-const Double_t T[] = {0.4};
+static const Int_t    P   = 0;
+static const Int_t    nN  = 1;
+static const Double_t N[] = {5.8};
+static const Double_t nT  = 1;
+static const Double_t T[] = {0.4};
 
 
 // jet parameters (won't impact unfolding)
-const Int_t    nRM     = 1;
-const Int_t    type    = 0;
-const Double_t rJet    = 0.3;
-const Double_t aMin    = 0.2;
-const Double_t pTmin   = 0.2;
-const Double_t eTmin   = 9.;
-const Double_t eTmax   = 20.;
-const Double_t hTrgMax = 0.9;
+static const Int_t    nRM     = 1;
+static const Int_t    type    = 0;
+static const Double_t rJet    = 0.3;
+static const Double_t aMin    = 0.2;
+static const Double_t pTmin   = 0.2;
+static const Double_t eTmin   = 9.;
+static const Double_t eTmax   = 20.;
+static const Double_t hTrgMax = 0.9;
 
 // these don't need to be changed
-const Int_t    beam   = 0;       // 0 = "pp", 1 = "AuAu"
-const Int_t    trig   = 2;       // 0 = "gamma-dir", 1 = "gamma-rich", 2 = "pi0"
-const Int_t    nToy   = 10;      // used to calculate covariances
-const Int_t    nMC    = 100000;  // number of MC iterations for backfolding
-const Double_t energy = 200.;    // sqrt(s)
-const Double_t bPrior = 0.1;     // normalization of prior
-const Double_t mPrior = 0.140;   // m-parameter of prior
+static const Int_t    beam   = 0;       // 0 = "pp", 1 = "AuAu"
+static const Int_t    trig   = 2;       // 0 = "gamma-dir", 1 = "gamma-rich", 2 = "pi0"
+static const Int_t    nToy   = 10;      // used to calculate covariances
+static const Int_t    nMC    = 100000;  // number of MC iterations for backfolding
+static const Double_t energy = 200.;    // sqrt(s)
+static const Double_t bPrior = 0.1;     // normalization of prior
+static const Double_t mPrior = 0.140;   // m-parameter of prior
 
 
 void DoUnfolding() {
@@ -117,18 +120,30 @@ void DoUnfolding() {
   sChi2 += ".performance.root";
 
   TFile *fChi2 = new TFile(sChi2.Data(), "recreate");
-  TH1D  *hBayUnfold    = new TH1D(sBayU.Data(), "Bayesian Algorithm", nK, K[0], K[nK - 1]);
-  TH1D  *hBayBackfold  = new TH1D(sBayB.Data(), "Bayesian Algorithm", nK, K[0], K[nK - 1]);
-  TH1D  *hSvdUnfold    = new TH1D(sSvdU.Data(), "SVD Algorithm", nK, K[0], K[nK - 1]);
-  TH1D  *hSvdBackfold  = new TH1D(sSvdB.Data(), "SVD Algorithm", nK, K[0], K[nK - 1]);
-  hBayUnfold   -> GetXaxis() -> SetName(sChiX.Data());
-  hBayUnfold   -> GetYaxis() -> SetName(sChiYU.Data());
-  hBayBackfold -> GetXaxis() -> SetName(sChiX.Data());
-  hBayBackfold -> GetYaxis() -> SetName(sChiYB.Data());
-  hSvdUnfold   -> GetXaxis() -> SetName(sChiX.Data());
-  hSvdUnfold   -> GetYaxis() -> SetName(sChiYU.Data());
-  hSvdBackfold -> GetXaxis() -> SetName(sChiX.Data());
-  hSvdBackfold -> GetYaxis() -> SetName(sChiYB.Data());
+  TH1D  *hBayUnfold    = new TH1D(sBayU.Data(), "Bayesian Algorithm", nK, K[0], K[nK - 1] + 1);
+  TH1D  *hBayBackfold  = new TH1D(sBayB.Data(), "Bayesian Algorithm", nK, K[0], K[nK - 1] + 1);
+  TH1D  *hSvdUnfold    = new TH1D(sSvdU.Data(), "SVD Algorithm", nK, K[0], K[nK - 1] + 1);
+  TH1D  *hSvdBackfold  = new TH1D(sSvdB.Data(), "SVD Algorithm", nK, K[0], K[nK - 1] + 1);
+  hBayUnfold   -> SetTitleFont(42);
+  hBayUnfold   -> GetXaxis() -> SetTitle(sChiX.Data());
+  hBayUnfold   -> GetXaxis() -> SetTitleFont(42);
+  hBayUnfold   -> GetYaxis() -> SetTitle(sChiYU.Data());
+  hBayUnfold   -> GetYaxis() -> SetTitleFont(42);
+  hBayBackfold -> SetTitleFont(42);
+  hBayBackfold -> GetXaxis() -> SetTitle(sChiX.Data());
+  hBayBackfold -> GetXaxis() -> SetTitleFont(42);
+  hBayBackfold -> GetYaxis() -> SetTitle(sChiYB.Data());
+  hBayBackfold -> GetYaxis() -> SetTitleFont(42);
+  hSvdUnfold   -> SetTitleFont(42);
+  hSvdUnfold   -> GetXaxis() -> SetTitle(sChiX.Data());
+  hSvdUnfold   -> GetXaxis() -> SetTitleFont(42);
+  hSvdUnfold   -> GetYaxis() -> SetTitle(sChiYU.Data());
+  hSvdUnfold   -> GetYaxis() -> SetTitleFont(42);
+  hSvdBackfold -> SetTitleFont(42);
+  hSvdBackfold -> GetXaxis() -> SetTitle(sChiX.Data());
+  hSvdBackfold -> GetXaxis() -> SetTitleFont(42);
+  hSvdBackfold -> GetYaxis() -> SetTitle(sChiYB.Data());
+  hSvdBackfold -> GetYaxis() -> SetTitleFont(42);
   hBayUnfold   -> Sumw2();
   hBayBackfold -> Sumw2();
   hSvdUnfold   -> Sumw2();
@@ -217,6 +232,58 @@ void DoUnfolding() {
       }  // end method loop
     }  // end tPrior loop
   }  // end nPrior loop
+
+
+  // make performance plots
+  const UInt_t cBay(810);
+  const UInt_t cSvd(860);
+  hBayUnfold   -> SetLineColor(cBay);
+  hBayUnfold   -> SetMarkerColor(cBay);
+  hBayBackfold -> SetLineColor(cBay);
+  hBayBackfold -> SetMarkerColor(cBay);
+  hSvdUnfold   -> SetLineColor(cSvd);
+  hSvdUnfold   -> SetMarkerColor(cSvd);
+  hSvdBackfold -> SetLineColor(cSvd);
+  hSvdBackfold -> SetMarkerColor(cSvd);
+
+  TLegend *lUnfold   = new TLegend(0.1, 0.1, 0.3, 0.3);
+  TLegend *lBackfold = new TLegend(0.1, 0.1, 0.3, 0.3);
+  lUnfold   -> SetFillColor(0);
+  lUnfold   -> SetLineColor(0);
+  lUnfold   -> SetTextFont(42);
+  lUnfold   -> SetTextAlign(12);
+  lUnfold   -> AddEntry(hBayUnfold, "Bayes.");
+  lUnfold   -> AddEntry(hSvdUnfold, "SVD");
+  lBackfold -> SetFillColor(0);
+  lBackfold -> SetLineColor(0);
+  lBackfold -> SetTextFont(42);
+  lBackfold -> SetTextAlign(12);
+  lBackfold -> AddEntry(hBayBackfold, "Bayes.");
+  lBackfold -> AddEntry(hSvdBackfold, "SVD");
+
+  TLine *lOne = new TLine(K[0], 1, K[nK - 1] + 1, 1);
+  lOne  -> SetLineColor(1);
+  lOne  -> SetLineStyle(2);
+  fChi2 -> cd();
+
+  TCanvas *cUnfold   = new TCanvas("cUnfold", "", 750, 500);
+  TCanvas *cBackfold = new TCanvas("cBackfold", "", 750, 500);
+  cUnfold      -> SetGrid(0, 0);
+  cBackfold    -> SetGrid(0, 0);
+  cUnfold      -> cd();
+  hBayUnfold   -> Draw();
+  hSvdUnfold   -> Draw("same");
+  lUnfold      -> Draw();
+  lOne         -> Draw();
+  cBackfold    -> cd();
+  hBayBackfold -> Draw();
+  hSvdBackfold -> Draw("same");
+  lBackfold    -> Draw();
+  lOne         -> Draw();
+  cUnfold      -> Write();
+  cUnfold      -> Close();
+  cBackfold    -> Write();
+  cBackfold    -> Close();
 
 
   // save chi2
